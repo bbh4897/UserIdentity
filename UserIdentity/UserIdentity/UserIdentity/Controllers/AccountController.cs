e92ffbf525e76f4ab37c24bd24e30aafa3ef8fa6
bbh4897 <bagbrc@gmail.com>
2018-12-09 19:45:14 +0300

    Sıfırdan Web Gelistirme Identity Uygulaması

---
+++ UserIdentity/UserIdentity/UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Identity;
+using UserIdentity.Models;
+
+namespace UserIdentity.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppIdentityUser> userManager;
+
+        public AccountController()
+        {
+            var userStore = new UserStore<AppIdentityUser>(new AppIdentityDbContext());
+            userManager = new UserManager<AppIdentityUser>(userStore);
+            userManager.UserValidator = new UserValidator<AppIdentityUser>(userManager)
+            {
+                RequireUniqueEmail = true
+            };
+        }
+
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppIdentityUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+
+                var result = userManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "users");
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View("Error", new string[] { "Erişim hakkınız yoktur" });
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userManager.Find(model.UserName, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Yanlış Kullanıcı Adı veya Parola");
+                }
+                else
+                {
+                    var authManager = HttpContext.GetOwinContext().Authentication;
+                    var identity = userManager.CreateIdentity(user, "ApplicationCookie");
+                    var authProperties = new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    };
+
+                    authManager.SignOut();
+                    authManager.SignIn(authProperties, identity);
+
+                    return Redirect(string.IsNullOrEmpty (returnUrl) ? "/" : returnUrl);
+                }
+            }
+
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return RedirectToAction("Login");
+        }
+
+
+    }
+}
---
+++ UserIdentity/UserIdentity/UserIdentity/Models/UserModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using UserIdentity.Identity;
+
+namespace UserIdentity.Models
+{
+    public class Register
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+       
+        [Required]
+        public string Password { get; set; }
+    }
+
+
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppIdentityUser> Members { get; set; }
+        public IEnumerable<AppIdentityUser> NonMembers { get; set; }
+    }
+
+
+    public class RoleUpdateModel
+    {
+        public string RoleName { get; set; }
+        public string RoleID { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+
+
+}
